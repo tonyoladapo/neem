a7441430ff06eea8ae6c9de9d3d206c63cf6212f
Tony <toneyobaze@gmail.com>
2022-04-26 15:52:07 +0000

    add get controller

--- src/controllers/deletePlanController.js
+++ src/controllers/deletePlanController.js
@@ -1,5 +1,14 @@
+const planModel = require("../models/planModel");
+
 const deletePlanController = (req, res) => {
-  res.send("deletePlanController");
+  const id = req.params.id;
+
+  const deletedPlan = await planModel.findOneAndDelete({ _id: id });
+
+  res.send({
+    message: "plan deleted",
+    data: deletedPlan,
+  });
 };
 
 module.exports = deletePlanController;
--- src/controllers/getPlanController.js
+++ src/controllers/getPlanController.js
@@ -1,5 +1,15 @@
+const planModel = require("../models/planModel");
+
 const getPlanController = (req, res) => {
-  res.send("getPlanController");
+  const id = req.params.id;
+
+  if (id) {
+    const plan = await planModel.findById({ _id: id });
+    res.send(plan);
+  } else {
+    const plans = await planModel.find();
+    response.send(plans);
+  }
 };
 
 module.exports = getPlanController;
